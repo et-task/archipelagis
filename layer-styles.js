35a9fd757c9e9ee0f9a098b55358caef61bc981d
Minna Palm <mppalm@utu.fi>
2025-11-03 12:11:37 +0200

    marker size changing to zoom level

--- layer-styles.js
+++ layer-styles.js
@@ -21,13 +21,29 @@ const layerStyles = {
   }
 };
 
+//marker style for service point data
 var medicalIcon = L.icon({
   className: 'medical-marker',
   iconUrl: './house_marker.png', //url of the medical point png
 
-  iconSize:     [30, 30], // size of the icon
+  iconSize:     [40, 40], // size of the icon
   iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+//house_marker.png source:
+//<a href="https://www.flaticon.com/free-icons/maps-and-location" title="maps and location icons">Maps and location icons created by juicy_fish - Flaticon</a>
+
+//to change the marker size according to zoom level
+map.on('zoom', function() {
+  const zoom = map.getZoom();
+  const scale = Math.pow(5, zoom - 10); 
+  const markerElems = document.getElementsByClassName('medical-marker');
+
+  for (let elem of markerElems) {
+    elem.style.transform = `scale(${scale})`;
+    elem.style.transformOrigin = 'center';
+  }
 });
 
 //html: '<div style="background-color: red; width: 10px; height: 10px; border-radius: 50%; border: 3px solid white; box-shadow: 0 0 5px black;"></div>',
